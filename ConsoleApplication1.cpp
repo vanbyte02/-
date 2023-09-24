0da5fa90bdca2f8e4e8a1f46bb2ff5f6f2174217
IvAn5 <IvAn5@DESKTOP-3HTCFOV>
2023-09-24 19:16:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int numbers1;
+	cout << "Введите число 1:\n";
+	cin >> numbers1;
+
+	char znak;
+	cout << "Введите знак:\n";
+	cin >> znak;
+
+	int numbers2;
+	cout << "Введите число 2:\n";
+	cin >> numbers2;
+
+	switch (znak) {
+
+	case '+':
+		cout << endl << numbers1 + numbers2;
+		break;
+
+	case '-':
+		cout << endl << numbers1 - numbers2;
+		break;
+
+	case '*':
+		cout << endl << numbers1 * numbers2;
+		break;
+
+	case '/':
+		if (numbers2 != 0) {
+			cout << endl << numbers1 / numbers2;
+		}
+		else 
+			cout << endl << "Ты че дебик? На ноль делить нельзя!";
+		break;
+	}
+}
